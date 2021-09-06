b75058ba99232e8686105ce6ab5eabe31c3e6480
keanu <keanu@inernetbs.net>
2021-09-06 18:40:47 +0300

    add Cart Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Header} from "./components";
-import {Home} from "./pages";
+import { Header } from "./components";
+import { Home, Cart } from "./pages";
+import {Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Home />
+        <Route path="/" component={Home} exact />
+        <Route path="/cart" component={Cart} exact />
       </div>
     </div>
   );
